cb32baaffc9f21b2df42ddeea4d0d2bc4fa673e5
Yanick Rochon <yanick.rochon@gmail.com>
2014-02-21 23:26:11 -0500

    First commit

---
+++ test/init.js
@@ -0,0 +1,10 @@
+
+if (!process.env.TRAVIS) {
+  if (typeof __cov === 'undefined') {
+    process.on('exit', function () {
+      require('semicov').report();
+    });
+  }
+
+  require('semicov').init('lib');
+}
